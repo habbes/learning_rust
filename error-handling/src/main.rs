08bda523a5154346e8b7537674570ba0c606a544
Clement Habinshuti <clhabins@microsoft.com>
2023-02-14 10:46:24 +0300

    Error handling

---
+++ error-handling/src/main.rs
@@ -0,0 +1,29 @@
+use std::fs::File;
+use std::io::{self, Read, ErrorKind};
+
+fn main() {
+    let greeting_file_result = File::open("hello.text");
+
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => File::create("hello.text").unwrap_or_else(|e| {
+                panic!("Problem creating the file: {:?}", e);
+            }),
+            other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+            }
+        }
+    };
+
+    // File::open("hello.text").expect("hello.text should be included in this project");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    // use ? as shortcut to return Err Result if call fails,
+    // saves us from using match Ok, Err for this common scenario
+    let mut username_file = File::open("hello.txt")?;
+    let mut username = String::new();
+    username_file.read_to_string(&mut username)?;
+    Ok(username)
+}
