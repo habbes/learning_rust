28ddbe05daea08ebf917ed89e77e2b945096f7f7
Clement Habinshuti <clhabins@microsoft.com>
2023-02-27 18:55:36 +0300

    Multiple owners of mutable data with Rc<T> and RefCell<T>

---
+++ cons-list-shared-mutable/src/main.rs
@@ -0,0 +1,30 @@
+
+// We combine Rc<T> and RefCell<T> to have multiple
+// owners who can mutate data
+// Note: Neither Rc<T> nor RefCell<T> can be used in a multithreaded environment
+
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+use std::cell::RefCell;
+
+fn main() {
+    let value = Rc::new(RefCell::new(5));
+
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    // we dereference the value and modify it
+    *value.borrow_mut() += 10;
+
+    println!("a after = {:?}", a);
+    println!("b after = {:?}", b);
+    println!("c after = {:?}", c);
+}
