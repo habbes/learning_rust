1d88d352067a3acb055b912f46dc7f25d1ac65ac
Clement Habinshuti <clhabins@microsoft.com>
2023-02-28 19:07:20 +0300

    Memory leaks due to cyclic references

---
+++ cons-list-cycle-memleak/src/main.rs
@@ -0,0 +1,46 @@
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug)]
+enum List {
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+}
+
+impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, next) => Some(next),
+            Nil => None,
+        }
+    }
+}
+
+fn main() {
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b initial rc count = {}", Rc::strong_count(&b));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing b = {}", Rc::strong_count(&a));
+
+    // Uncomment next line to see that we have a cycle;
+    // it will overflow the stack
+    // println!("a next item = {:?}", a.tail());
+
+    // at the end of the scope, b will be dropped, reducing the ref count of its list to 1
+    // a will be dropped reducing the ref count of its list to 1 as well
+    // the memory allocated for both lists on the heap will remain uncollected indefinitely: memory leak!
+}
